99a76dc43eb6122ce942ec8f8655b6d5f8a97440
Hudson Schumaker <hudson.schumaker@hotmail.com>
2017-07-06 22:34:50 -0300

    Ads endless;

---
+++ OfficeHero/Assets/Scripts/endless/HsEndless.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/* 
+	@author Hudson Schumaker
+	@version 1.0.0
+*/
+
+public class HsEndless : MonoBehaviour {
+
+	void Start () {
+		AdManager.instance.ShowBanner ();
+	}
+
+	void Update () {
+		
+	}
+}
